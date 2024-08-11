c7b7ac29d27643aa3a4e446e61f86f32d8d85389
Alvaro López mendiola <alvarolopezmendiola@gmail.com>
2024-08-11 09:46:02 -0400

    next commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"https://alvatronsyt.github.io/ticketsure-solidity/"
+  base:"/ticketsure-solidity/",
+  plugins: [react()]
 })
